312f9813959ea9403e90bf9fafef1f83ff746cf4
Mateusz Janek <stryku2393@gmail.com>
2017-09-10 02:31:59 +0200

    introduce tesseract

---
+++ tesseract/run_trainer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -r build
+mkdir -p build
+cd build
+
+python ../trainer.py
---
+++ tesseract/trainer.py
@@ -0,0 +1,69 @@
+from subprocess import call
+
+BUILD_DIR='build'
+FONTa='Fake Receipt'
+LANG='hb'
+OUTPUTBASE = LANG + '.' + FONTa
+
+
+def call_shell(command):
+    splitted = command.split()
+    call(splitted)
+    print(command)
+
+
+def text2image(text_file):
+    splitted = str('text2image --text=' + text_file + ' --fonts_dir ..').split()
+    splitted.append('--outputbase=' + OUTPUTBASE)
+    splitted.append('--font=Fake Receipt')
+    call(splitted)
+
+
+def training():
+    command = ['tesseract', OUTPUTBASE + '.tif', OUTPUTBASE, 'box.train.stderr']
+    call(command)
+
+
+def unicharset():
+    command = ['unicharset_extractor',
+               OUTPUTBASE + '.box']
+    call(command)
+
+
+def clustering():
+    command = ['mftraining',
+               '-F', '../font_properties',
+               '-U', 'unicharset',
+               OUTPUTBASE + '.tr']
+    call(command)
+
+
+def cntraining():
+    command = ['cntraining', OUTPUTBASE + '.tr']
+    call(command)
+
+
+def cp_with_prefix(filename, prefix):
+    call_shell('cp ' + filename + ' ' + prefix + '.' + filename)
+
+
+def prepare_for_combine():
+    cp_with_prefix('unicharset', LANG)
+    cp_with_prefix('shapetable', LANG)
+    cp_with_prefix('normproto', LANG)
+    cp_with_prefix('inttemp', LANG)
+    cp_with_prefix('pffmtable', LANG)
+
+
+def combine():
+    command = ['combine_tessdata', LANG + '.']
+    call(command)
+
+
+text2image('../training_text.txt')
+training()
+unicharset()
+clustering()
+cntraining()
+prepare_for_combine()
+combine()
