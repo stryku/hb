504e8d727a31e26f30d2da2320d910281233a3e4
stryku <stryku2393@gmail.com>
2017-09-30 20:34:51 -0700

    updated update_to_repo.sh

--- deploy/update_to_repo.sh
+++ deploy/update_to_repo.sh
@@ -4,11 +4,20 @@ REPO_URL='https://github.com/stryku/hb'
 DEPLOY_DIR=`pwd`
 BACKUP_DIR=$DEPLOY_DIR/..
 
-./backup $DEPLOY_DIR $BACKUP_DIR
+echo '[UPDATE TO REPO START]'
 
-cd $(mktemp -d)
+echo '[BACKUP]'
+./backup.sh $DEPLOY_DIR $BACKUP_DIR
+
+echo '[CLONE REPO]'
+REPO_DIR=$(mktemp -d)
+cd $REPO_DIR
 git clone $REPO_URL
-cd hb/deploy
-./deploy.sh $DEPLOY_DIR
-rm `pwd`
 
+echo '[DEPLOY]'
+$REPO_DIR/hb/deploy/deploy.sh $DEPLOY_DIR
+
+echo '[RM REPO DIRECTORY]'
+rm -rf $REPO_DIR
+
+echo '[UPDATE TO REPO END]'
